8237af51d38225c916ae7ce810e0289ce8aabdae
Bastian Benitez <92552627+BastianBenitez@users.noreply.github.com>
2023-10-13 18:33:40 -0300

    Actualiazacion de la ruta /teams
    La api es capas de dar un respuesta con los datos ordenados, para una falcil utilizacion de los datos.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
-const { createConnection } = require('./db.js'); // Importa la función de conexión
+const { createConnection } = require('./db.js');
 
 // Middleware
 app.use(morgan('dev'));
--- src/routes/respuesta.js
+++ src/routes/respuesta.js
@@ -16,11 +16,30 @@ router.get('/drivers', (req, resp) => {
 });
 
 router.get('/teams', (req, resp) => {
-    Connection.query('SELECT * from Teams ORDER BY Points DESC', (err, res) => {
+    Connection.query('SELECT  Teams.IdTeam, Drivers2023T2.NameD, Teams.NameT,Teams.PointsT FROM Drivers2023T2 INNER JOIN Teams ON Drivers2023T2.Team = Teams.IdTeam ORDER BY PointsT DESC;', (err, res) => {
         if (err) {
             console.log(err);
         } else {
-            resp.json(res);
+            const groupedData = {};
+
+            res.forEach(item => {
+            const teamId = item.IdTeam;
+            if (!groupedData[teamId]) {
+                groupedData[teamId] = {
+                IdTeam: teamId,
+                NameT: item.NameT,
+                PointsT: item.PointsT,
+                NameB: [item.NameD],
+                };
+            } else {
+                groupedData[teamId].NameB.push(item.NameD);
+            }
+            });
+
+            const result = Object.values(groupedData);
+            result.sort((a, b) => b.PointsT - a.PointsT);
+            console.log(result);
+            resp.json(result);
         }
     });
 });
